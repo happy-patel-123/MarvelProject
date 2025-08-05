8eb412deaf02590d4f6fa4d1e3e82078e0649b36
happy-patel-123 <happypatel9924@gmail.com>
2025-08-05 23:38:22 +0530

    IMP NOTE: Make Notes of Random Concepts, no need to follow a pattern. (You can find in Googly, you can in 200 pages book, handwrittern by you.)

--- MarvelProject/src/navigation/Navigation.tsx
+++ MarvelProject/src/navigation/Navigation.tsx
@@ -15,7 +15,7 @@ const Navigation = () => {
                 <Stack.Screen name="Splash" component={Splash} />
                 <Stack.Screen name="Login" component={Login} /> 
                 <Stack.Screen name="SignUp" component={SignUp} /> 
-                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Home" component={Home} options={{ headerShown: true }}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
